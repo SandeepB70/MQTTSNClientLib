6590566ab1862b6f2956c14ea7a8e22088e2b3c6
Sandeep <sbindra70@gmail.com>
2020-10-04 14:19:01 -0400

    Added MQTTSNPacket folder and modded ReadMe.md

---
+++ MQTTSNPacket/samples/CMakeFiles/qos-1pub_extended.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/MQTTSNPacket/samples/qos-1pub_extended.c" "/tmp/MQTTSNPacket/samples/CMakeFiles/qos-1pub_extended.dir/qos-1pub_extended.o"
+  "/tmp/MQTTSNPacket/samples/transport.c" "/tmp/MQTTSNPacket/samples/CMakeFiles/qos-1pub_extended.dir/transport.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Library for implementing MQTTSN client in C.
